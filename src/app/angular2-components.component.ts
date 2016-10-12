b13c9ec000bf622f57b4144790c706e9b5227f86
Lewis Kabui <lewis.maina@andela.com>
2016-10-12 16:10:05 +0300

    Create Angular2ComponentsAppComponent component

--- src/app/angular2-components.component.ts
+++ src/app/angular2-components.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
 
 @Component({
-  moduleId: module.id,
-  selector: 'angular2-components-app',
-  templateUrl: 'angular2-components.component.html',
-  styleUrls: ['angular2-components.component.css']
+  // moduleId: module.id,
+  // selector: 'angular2-components-app',
+  // templateUrl: 'angular2-components.component.html',
+  // styleUrls: ['angular2-components.component.css']
+  selector: 'my-app',
+  styles: [`
+    h1 {
+        color: #545454;
+        background: #02a8f4;
+        padding: 15px;
+        box-shadow:2px 2px 2px 0 rgba(0, 0, 0, 0.3);
+    }
+  `],
+  template: `
+    <h1>Hello from the {{ componentName }}.</h1>
+  `
 })
 export class Angular2ComponentsAppComponent {
-  title = 'angular2-components works!';
+  // title = 'angular2-components works!';
+  componentName = 'AppComponent'
 }
+
+bootstrap(Angular2ComponentsAppComponent);
